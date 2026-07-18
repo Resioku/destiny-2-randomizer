763914c6549a2e6cd943f8a5aa13c6bccbe3a302
Resioku <82930426+Resioku@users.noreply.github.com>
2026-07-18 14:35:18 -0400

    Update notes.js

--- notes.js
+++ notes.js
@@ -4,5 +4,5 @@
 
 const NOTES_TEXT = `
 - Prismatic is marked [WIP] and off by default in Filters. It technically can use a lot of non-Prismatic exotics, but
-  that list isn't built out yet.
+  that list isn't built out yet. 
 `;
